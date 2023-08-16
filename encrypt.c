efc309e0bc598cacdd6d7fd85112895f9eb0f252
it22597274 <it22597274@my.sliit.lk>
2023-08-17 00:25:31 +0530

    c update

--- encrypt.c
+++ encrypt.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
+void encrypt(char *text, int key) {
+    for (int i = 0; i < strlen(text); i++) {
+        if (isalpha(text[i])) {
+            int shift = key % 26;
+            if (islower(text[i])) {
+                text[i] = (((text[i] - 'a' + shift) % 26) + 'a');
+            } else {
+                text[i] = (((text[i] - 'A' + shift) % 26) + 'A');
+            }
+        }
+    }
+}
+
+void decrypt(char *text, int key) {
+    for (int i = 0; i < strlen(text); i++) {
+        if (isalpha(text[i])) {
+            int shift = key % 26;
+            if (islower(text[i])) {
+                text[i] = (((text[i] - 'a' - shift + 26) % 26) + 'a');
+            } else {
+                text[i] = (((text[i] - 'A' - shift + 26) % 26) + 'A');
+            }
+        }
+    }
+}
+
+int main() {
+    char plaintext[] ="";
+    int key =2;
+
+    printf("Enter the plaintext: ");
+    scanf("%s",plaintext);
+
+    encrypt(plaintext, key);
+    printf("Encrypted: %s\n", plaintext);
+
+    decrypt(plaintext, key);
+    printf("Decrypted: %s\n", plaintext);
 
     return 0;
 }
