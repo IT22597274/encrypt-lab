e5e8aab7b0103c91192d6bd4071e7e825a977b26
it22597274 <it22597274@my.sliit.lk>
2023-08-16 21:28:31 +0530

    encrypt

---
+++ encript.py
@@ -0,0 +1,10 @@
+need = srt(input("What do you want(E-encrypt/D-decrypt: )"))
+
+if (need == "E"):
+    planeT = str(input("Enter your plane Text: "))
+
+elif (need == "D"):
+    cihperT = str(input("Enter Your Plane Text:"))
+
+else:
+    printf("Error input")
